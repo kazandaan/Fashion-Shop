a754623ad41b37c99c29c1d4e8a95f7a065a338a
kazandaan <lianjaijie@gmail.com>
2020-10-16 21:25:25 +0800

    Initial commit

--- js/carousel.js
+++ js/carousel.js
@@ -28,3 +28,32 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
+//In testing phase, ignore this
+$(document).ready(function(){
+  var docEl = $(document),
+      headerEl = $('header'),
+      headerWrapEl = $('.wrapHead'),
+      navEl = $('nav'),
+      linkScroll = $('.scroll');
+
+  docEl.on('scroll', function(){
+    if ( docEl.scrollTop() > 60 ){
+      headerEl.addClass('fixed');
+      headerWrapEl.addClass('fixed');
+      navEl.addClass('fixed');
+    }
+    else {
+      headerEl.removeClass('fixed');
+      headerWrapEl.removeClass('fixed');
+      navEl.removeClass('fixed');
+    }
+  });
+
+  // linkScroll.click(function(e){
+  //     e.preventDefault();
+  //     $('body, html').animate({
+  //        scrollTop: $(this.hash).offset().top
+  //     }, 500);
+  //  });
+});
