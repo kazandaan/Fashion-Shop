46c51ee7998a0aa4473fabd739ea0a3159312696
nkhalisya <khalisyasulaiman@yahoo.com.sg>
2020-10-24 02:21:37 +0800

    2 tables

---
+++ tables/rating_randa.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 24, 2020 at 02:21 AM
+-- Server version: 5.5.44-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `f32ee`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating_randa`
+--
+
+CREATE TABLE IF NOT EXISTS `rating_randa` (
+  `rating_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `rating_favourite` tinyint(1) NOT NULL,
+  PRIMARY KEY (`rating_id`),
+  KEY `product_id` (`product_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `rating_randa`
+--
+ALTER TABLE `rating_randa`
+  ADD CONSTRAINT `rating_randa_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user_randa` (`user_id`),
+  ADD CONSTRAINT `rating_randa_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product_randa` (`product_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
