57d21ecfc569ea05ba0998ab0e00f97b235c6c73
nkhalisya <khalisyasulaiman@yahoo.com.sg>
2020-10-25 06:24:55 +0800

    a

--- account.php
+++ account.php
@@ -24,7 +24,7 @@
 <?php
   session_start();
   if(!isset($_SESSION['userid'])){
-    header("Location:index.php");
+    header("Location:index.php?page=unauthorized&status=2");
   }
   else{
     $id = $_SESSION['userid'];
