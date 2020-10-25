1fbdbaa0b4455f4493b3b1a4111a47cc1696dc85
nkhalisya <khalisyasulaiman@yahoo.com.sg>
2020-10-25 21:08:58 +0800

    Checkout Function

--- checkout.php
+++ checkout.php
@@ -7,6 +7,7 @@
   <link rel="stylesheet" href="css/general.css">
   <link rel="stylesheet" href="css/utility.css">
   <link rel="stylesheet" href="css/carousel.css">
+  <link rel="stylesheet" href="css/checkout.css">
   <link href="https://fonts.googleapis.com/css2?family=Italiana&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -34,79 +35,86 @@
   <!-- This generates nav and banner -->
   <?php include "html/top.php"; ?>
 
-  <section id="contact">
+  <?php
+    // Create connection (servername, username, password, dbname)
+    $conn = mysqli_connect("localhost", "f32ee", "f32ee", "f32ee");
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+  ?>
+
+  <section id="checkout">
+    <h1>Checkout Items</h1>
+    <hr>
+
     <div class="container-fluid">
-      <div class="row" style="padding: 3rem 0;">
-      <div class="col-12" >
-          <h2 style="font-size:40px;">Ask us anything.</h2>
-      </div>
-      <div class="col-7" style="border-radius: 6px; box-shadow:0px 8px 35px 10px #E8E8E8; padding:2rem 1rem 2rem 1rem;">
-          <form class="" action="" method="post">
-            <div class="form-group row">
-              <div class="col">
-                <label for="">First Name<span>&#42;</span></label><br>
-                <input type="text" class="form-control" name="firstname" id="firstname" value="" required>
-              </div>
-              <div class="col">
-                <label for="">Last Name<span>&#42;</span></label><br>
-                <input type="text" class="form-control" name="lastname" id="lastname" value="" required>
-              </div>
-            </div>
+      <form id="checkoutBtn" class="buttons" action="action/checkoutProducts.php" method="POST">
+        <table>
+          <tr>
+            <th>S/N</th>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+          <?php
+            $sql = "SELECT * FROM product_randa
+            JOIN cart_randa ON product_randa.product_id = cart_randa.product_id
+            WHERE user_id = $id";
+            $runsql = mysqli_query($conn, $sql);
+            // should not be empty
+            if( !mysqli_num_rows($runsql) > 0 ){
+              header("Location:products.php?page=checkout&status=2");
+            }
 
-            <div class="form-group row">
-              <div class="col">
-                <label for="">Email<span>&#42;</span></label><br>
-                <input type="text" class="form-control" ame="email" id="email" value="" required>
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <div class="col">
-                <label for="">Subject</label><br>
-                <input type="text" class="form-control" name="subject" id="subject" value="">
-              </div>
-            </div>
-
-            <div class="form-group row">
-              <div class="col">
-                <label for="">Message</label><br>
-                <textarea class="form-control" name="message" id="message" rows="8" cols="30" style="resize:none;"></textarea>
-              </div>
-
-            </div>
-
-            <div class="form-group row">
-                <div class="col">
-                  <input class="form-control btnSubmit" type="submit" id="submit" name="submit" value="Send Message" style="letter-spacing: 2px;">
-                </div>
-            </div>
-          </form>
-        </div>
-
-      <div class="col-5 contact-icon">
-          <div class="contact-holder flex" >
-            <i class="fas fa-map-marker-alt"></i>
-            <p>Hougang Central 530837 Singapore.</p>
-          </div>
-          <div class="contact-holder flex" >
-            <i class="fas fa-phone-alt"></i>
-            <p>+65 87141256</p>
-          </div>
-          <div class="contact-holder flex">
-            <i class="far fa-envelope"></i>
-            <p>ntu@gmail.com</p>
-          </div>
-        </div>
-      </div>
-
+            $sn = 0;
+            $totalPrice = 0;
+            while ($product = mysqli_fetch_assoc($runsql)) {
+              $sn++;
+              $price2dp = number_format((float)$product['product_price'], 2);
+              $total = (float) $product['product_price'] * $product['quantity'];
+              $totalPrice += $total;
+          ?>
+          <tr>
+            <td><?php echo $sn; ?></td>
+            <td><?php echo $product['product_name']; ?></td>
+            <td>$<?php echo $price2dp . " x " . $product['quantity']; ?></td>
+            <td>$<?php echo number_format($total, 2); ?></td>
+          </tr>
+          <?php
+            }
+          ?>
+          <tr>
+            <th colspan="3" style="text-align:right;">Total</th>
+            <th>$<?php echo number_format($totalPrice, 2); ?></th>
+          </tr>
+          <tr>
+            <td class="last" colspan="4">
+              <!-- Hidden values -->
+              <input type="hidden" name="userid" value="<?php echo $id; ?>"/>
+              <input type="hidden" name="totalprice" value="<?php echo $totalPrice; ?>"/>
+              <!-- Checkout Button -->
+              <input type="submit" value="CONFIRM CHECKOUT"/>
+            </td>
+          </tr>
+          <tr>
+            <td class="last" colspan="5">Date: <?php echo date("d-m-Y"); ?></td>
+          </tr>
+        </table>
+      </form>
     </div>
   </section>
 
-  <!-- This generates modal -->
-  <?php echo file_get_contents("html/modal.html"); ?>
+  <!-- Popup Block -->
+  <div class="messagePopup" id="updateStatus">
+    <h2 id="messageHeader"></h2>
+  </div>
+
+  <?php
+    mysqli_close($conn);
+  ?>
 
   <!-- This generates footer -->
   <?php echo file_get_contents("html/bottom.html"); ?>
-
-  <script type="text/javascript" src="js/modal.js"></script> <!-- Modal script -->
 </body>
